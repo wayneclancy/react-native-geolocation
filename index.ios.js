286d18b7a08312a7880d05586dc956bf68e3ab90
Andrei Rosca <contact@arosca.ro>
2019-01-21 10:54:22 +0200

    proxy methods to BackgroundGeolocation

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+import BackgroundGeolocation as BG from 'react-native-background-geolocation';
+
+// https://transistorsoft.github.io/react-native-background-geolocation/classes/_react_native_background_geolocation_.backgroundgeolocation.html#ready
+export const ready = BG.ready;
+
+// https://transistorsoft.github.io/react-native-background-geolocation/classes/_react_native_background_geolocation_.backgroundgeolocation.html#startgeofences
+export const startGeofences = BG.startGeofences;
+
+// https://transistorsoft.github.io/react-native-background-geolocation/classes/_react_native_background_geolocation_.backgroundgeolocation.html#stop
+export const stop = BG.stopGeofences;
+
+// https://transistorsoft.github.io/react-native-background-geolocation/classes/_react_native_background_geolocation_.backgroundgeolocation.html#addgeofences
+export const addGeofences = BG.addGeofences;
+
+// https://transistorsoft.github.io/react-native-background-geolocation/classes/_react_native_background_geolocation_.backgroundgeolocation.html#ongeofence
+exprot const onGeofence = BG.onGeofence;
