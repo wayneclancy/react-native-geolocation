98b05a0ab3afe7de0cdce866fa3e694754764acb
Konrad Górski <konrad.gorski@purepoint.io>
2019-01-30 14:54:38 +0100

    Add notification for headless JS task

--- android/src/main/java/co/uk/hive/reactnativegeolocation/geofence/GeofenceHeadlessJsTaskService.java
+++ android/src/main/java/co/uk/hive/reactnativegeolocation/geofence/GeofenceHeadlessJsTaskService.java
@@ -1,18 +1,25 @@
 package co.uk.hive.reactnativegeolocation.geofence;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import co.uk.hive.reactnativegeolocation.RNMapper;
 import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+import uk.co.centrica.hive.reactnativegeolocation.R;
 
 public class GeofenceHeadlessJsTaskService extends HeadlessJsTaskService {
 
     private static final String HEADLESS_TASK_NAME = "GeofenceEventTask";
     private static final String HEADLESS_TASK_ARGUMENT_NAME = "geofence";
+    public static final int NOTIFICATION_ID = 12;
+    public static final String NOTIFICATION_CHANNEL_ID = "notification_channel_headless_task_notification";
 
     private RNMapper mRnMapper;
 
@@ -20,6 +27,12 @@ public class GeofenceHeadlessJsTaskService extends HeadlessJsTaskService {
     public void onCreate() {
         super.onCreate();
         mRnMapper = new RNMapper();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        showOreoNotification();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -42,5 +55,19 @@ public class GeofenceHeadlessJsTaskService extends HeadlessJsTaskService {
         HeadlessJsTaskService.acquireWakeLockNow(context);
         ContextCompat.startForegroundService(context, intent);
     }
-}
 
+    private void showOreoNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            if (nm != null && nm.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null) {
+                nm.createNotificationChannel(new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                        getString(R.string.headless_notification_channel_name),
+                        NotificationManager.IMPORTANCE_MIN));
+            }
+            startForeground(NOTIFICATION_ID, new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setContentText(getString(R.string.headless_notification_processing))
+                    .build());
+        }
+    }
+}
