2be088e37bc119b4ae653b918372cc6477e63cf0
Andrey Tsurikov <andrey.tsurikov@dataart.com>
2019-08-30 16:58:44 +0300

    iOS native module for geolocation

---
+++ ios/RNGeolocation/RNGeolocation.m
@@ -0,0 +1,13 @@
+//
+//  RNGeolocation.m
+//  RNGeolocation
+//
+//  Created by Andrew on 30/08/2019.
+//  Copyright © 2019 Centrica. All rights reserved.
+//
+
+#import "RNGeolocation.h"
+
+@implementation RNGeolocation
+
+@end
